7d0e8855007bc8d4178b8e79c5d4039c19e0f012
EErickson <avilasonson@gmail.com>
2023-06-13 02:58:03 +0800

    api call using free fake rest api

--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 const Dashboard = () =>{
+    const [users, setUsers ] = useState([]);
+    useEffect(() => {
+        fetchData();
+    },[])
+
+    const fetchData = async () =>{
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const jsonData = await response.json();
+        setUsers(jsonData);
+    }
+
+    if(users.length == 0){
+        return <div>Loading....</div>
+    }
+
     return (
         <div>
-            <h1>Hello!!</h1>
+            <h1>User List:</h1>
+            <ul>
+                {users.map((user)=>(
+                    <li key= {user.id}>{user.name}</li>
+                ))}
+            </ul>
         </div>
     );
 }
