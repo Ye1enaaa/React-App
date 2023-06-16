bddefc220325c7faa12a5e0f343a72f6b9b1800a
EErickson <avilasonson@gmail.com>
2023-06-16 18:54:39 +0800

    login page added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Dashboard from './dashboard/dashboard.jsx';
 import Register from './auth/register';
+import Login from './auth/login';
 function App() {
   return (
     <Routes>
       <Route path="/" exact element={<Register/>}></Route>
+      <Route path="/login" exact element={<Login/>}></Route>
       <Route path="/dashboard" exact element={<Dashboard/>}></Route>
     </Routes>
   );
