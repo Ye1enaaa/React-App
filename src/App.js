f9e82b0a7abd9a293804bbb364c7a6e61d8849a6
EErickson <avilasonson@gmail.com>
2023-06-16 19:05:59 +0800

    add userlists page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import './App.css';
 import Dashboard from './dashboard/dashboard.jsx';
 import Register from './auth/register';
 import Login from './auth/login';
+import UserListPreview from './user_list/userlist';
 function App() {
   return (
     <Routes>
       <Route path="/" exact element={<Register/>}></Route>
       <Route path="/login" exact element={<Login/>}></Route>
-      <Route path="/dashboard" exact element={<Dashboard/>}></Route>
+      <Route path="/dashboard" exact element={<Dashboard/>}>
+        <Route path="users" exact element={<UserListPreview/>}></Route>
+      </Route>
     </Routes>
   );
 }
